4d6cc47963391a9d5433238ed9cab0fd832a435d
ukituki <ukituki@gmail.com>
2014-04-19 00:56:41 +0200

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,17 +2,17 @@
 ## matrices. It's done by calling cacheSolve() function on the output of 
 ## a constructor function makeCacheMatrix(), eg:
 #
-matrixX <- matrix(rnorm(16), 4,4,)
-y <- makeCacheMatrix(matrixX)   #1. matrix is saved to and can be retrived later 
-                                # by $get() method
-                                #2. A list of 4 elements is returned 
-                                #   (outputs of: get(), set(), getinverse(), 
-                                #                setinverse()).        
-cacheSolve(y)           # 1. when called for the first time calculate new 
-                        # new inverse matrix using solve(matrixX) function
-                        # 2. when called another time instead of repeating
-                        # the same calculation for the same matrix, return saved
-                        # result
+# matrixX <- matrix(rnorm(16), 4,4,)
+# y <- makeCacheMatrix(matrixX)   #1. matrix is saved to and can be retrived later 
+#                                 # by $get() method
+#                                 #2. A list of 4 elements is returned 
+#                                 #   (outputs of: get(), set(), getinverse(), 
+#                                 #                setinverse()).        
+# cacheSolve(y)           # 1. when called for the first time calculate new 
+#                         # new inverse matrix using solve(matrixX) function
+#                         # 2. when called another time instead of repeating
+#                         # the same calculation for the same matrix, return saved
+#                         # result
 
 
 ## stores matrix and inverse of a matrix after it's passed from cacheSolve() 
@@ -32,7 +32,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## First, check if the inverse matrix has already calculated, 
-## Then,  
+## Then, return it or calculate and save with makeCacheMatrix(x)$setinverse(inv)
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
